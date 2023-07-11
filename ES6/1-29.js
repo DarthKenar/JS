71cd96bac93cf032533d6cf8a32e1a1cc631e17e
Federico Vega - HP <federico_Vega22@hotmail.com>
2023-07-11 18:37:51 -0300

    ej 1

---
+++ ES6/1-29.js
@@ -0,0 +1,9 @@
+var VAR = "Hola ";
+let LET = "Fede";
+
+function contatenacion(VAR,LET){
+    var VAR = "Chau "
+    // let LET = "Luci"
+    return VAR + LET;
+}
+console.log(contatenacion(VAR,LET));
