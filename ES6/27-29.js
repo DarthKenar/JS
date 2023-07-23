553ab565043f40507c3eefab821a8602b1230ba8
DarthKenar <federico_vega22@hotmail.com>
2023-07-22 22:39:59 -0300

    20-29 to 29-29 exercise added

---
+++ ES6/27-29.js
@@ -0,0 +1,11 @@
+const makeServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer representa una respuesta de un servidor
+    let responseFromServer;
+  
+    if(responseFromServer) {
+      resolve("We got the data")
+    } else {  
+      reject("Data not received")
+      // Cambia esta línea
+    }
+  });
