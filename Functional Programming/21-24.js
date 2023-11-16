976355a68a8f1733ccfc4f84d20fc98adf7e5747
Federico <federico_vega22@hotmail.com>
2023-11-16 12:42:57 -0300

    Functional Programming Complete

--- Functional Programming/21-24.js
+++ Functional Programming/21-24.js
@@ -1,8 +1,9 @@
 // Cambia solo el código debajo de esta línea
 function urlSlug(title) {
-    let arrTitle = title.split(" ").map((str) => str.toLowerCase())
-    let str = arrTitle.join("-")
-    return str
-  }
-  // Cambia solo el código encima de esta línea
-  urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone");
+  let arrTitle = title.split(/['" ]/).map((str) => str.toLowerCase())
+  let arrFiltered = arrTitle.filter((str) => str != "")
+  let str = arrFiltered.join("-")
+  return str
+}
+// Cambia solo el código encima de esta línea
+urlSlug(" Winter Is  Coming")
