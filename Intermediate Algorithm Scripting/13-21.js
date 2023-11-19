08af089e18d0e61973dcaf20c35da43a30143866
Federico <federico_vega22@hotmail.com>
2023-11-19 18:23:38 -0300

    primes WIP

--- Intermediate Algorithm Scripting/13-21.js
+++ Intermediate Algorithm Scripting/13-21.js
@@ -1,31 +1,18 @@
 function sumPrimes(num) {
-    // Función para generar la criba de Eratóstenes
-    function generatePrimesSieve(limit) {
-        const sieve = Array(limit + 1).fill(true);
-        sieve[0] = sieve[1] = false;
+    //generar todos los numeros desde 0 a num
+    let primes = []
+    for(let i=0;i<=num;i++){
+      primes.push(i)
+    };
+    //comprobar si son primos a partír del 2
+    for(let i=2;i>num; i++){
+      if(primes.includes(i)){
+        primes.filter((e)=>e%i==0)
+        console.log(primes)
+      };
+    };
+    console.log(primes)
 
-        for (let i = 2; i <= Math.sqrt(limit); i++) {
-            if (sieve[i]) {
-                for (let j = i * i; j <= limit; j += i) {
-                    sieve[j] = false;
-                }
-            }
-        }
-        return sieve;
-    }
-
-    // Generar la criba de Eratóstenes hasta el número dado
-    const primesSieve = generatePrimesSieve(num);
-
-    // Sumar los números primos generados por la criba
-    let sum = 0;
-    for (let i = 2; i <= num; i++) {
-        if (primesSieve[i]) {
-            sum += i;
-        }
-    }
-
-    return sum;
 }
 
-console.log(sumPrimes(977)); // Debería devolver 73156
+console.log(sumPrimes(10)); // Debería devolver 73156
