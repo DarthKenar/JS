21d5b16ca57bccd85e489b1d4a0b32bbb5e20e9b
Federico Vega <federico_vega22@hotmail.com>
2023-11-20 15:51:48 -0300

    Intermediate Algorithm

---
+++ Intermediate Algorithm Scripting/16-21.js
@@ -0,0 +1,8 @@
+// recusion mode
+function steamrollArray(val,flatArr=[]) {
+    val.forEach(item => {
+      if (Array.isArray(item)) steamrollArray(item, flatArr);
+      else flatArr.push(item);
+    });
+    return flatArr;
+  }
