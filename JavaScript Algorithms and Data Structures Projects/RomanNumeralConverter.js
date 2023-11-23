374f82b29adaabba845679faf10792406b7f46f3
Federico Vega <federico_vega22@hotmail.com>
2023-11-23 17:42:06 -0300

    Complete RomanNumeralConverter

--- JavaScript Algorithms and Data Structures Projects/RomanNumeralConverter.js
+++ JavaScript Algorithms and Data Structures Projects/RomanNumeralConverter.js
@@ -1,23 +1,31 @@
 function convertToRoman(num) {
-    let romanNumbers = {
-      M: 1000,
-      CM: 900,
-      D: 500,
-      CD: 400,
-      C: 100,
-      XC: 90,
-      L: 50,
-      XL: 40,
-      X: 10,
-      IX: 9,
-      V: 5,
-      IV: 4,
-      I: 1,
-    }
-    for(let i=0; i<num.length; i++){
-  
-    }
-    return num;
+  let romanValue = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
   }
-  
-  convertToRoman(36);
+  let respuesta = ""
+  let romanKeysIndex = Object.keys(romanValue)
+  let romanKeysLength = romanKeysIndex.length
+  while(num>0){
+    for(let i=0; i<romanKeysLength; i++){
+      if(romanValue[romanKeysIndex[i]]<=num){
+        num -= romanValue[romanKeysIndex[i]]
+        respuesta = respuesta.concat(romanKeysIndex[i])
+        break;
+      }
+    }
+  }
+  return respuesta;
+}
+convertToRoman(2);
