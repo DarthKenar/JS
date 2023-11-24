6df73c8e7c0c28bb0d110a347582f786b6111bd3
Federico <federico_vega22@hotmail.com>
2023-11-24 14:54:20 -0300

    CashRegister

--- JavaScript Algorithms and Data Structures Projects/CashRegister.js
+++ JavaScript Algorithms and Data Structures Projects/CashRegister.js
@@ -1,10 +1,50 @@
 function checkCashRegister(price, cash, cid) {
-    let change = cash - price;
+    let totalChange = cash - price;
+    let values = [
+  ["PENNY", 0.01],
+  ["NICKEL", 0.05],
+  ["DIME", 0.1],
+  ["QUARTER", 0.25],
+  ["ONE", 1],
+  ["FIVE", 5],
+  ["TEN", 10],
+  ["TWENTY", 20],
+  ["ONE HUNDRED", 100]
+]
+    let change = [
+  ["PENNY", 0.00],
+  ["NICKEL", 0.00],
+  ["DIME", 0.0],
+  ["QUARTER", 0.0],
+  ["ONE", 0],
+  ["FIVE", 0],
+  ["TEN", 0],
+  ["TWENTY", 0],
+  ["ONE HUNDRED", 0]
+]
+let valueslength = values.length-1
+console.log(values[valueslength][1])
+    while(totalChange != 0){
+      for(let i=0; i<valueslength;i++){
+        if(values[valueslength-i][1]<=totalChange){
+          if(cid[valueslength-i][1]>0){
+            cid[valueslength-i][1] -= values[valueslength-i][1]
+            totalChange -= values[valueslength-i][1]
+            change[valueslength-i][1] += values[valueslength-i][1]
+            break;
+          };
+        };
+        //si esta en la comprobación del ultimo elemento y el valor change no es 0
+          //  devolver {status: "INSUFFICIENT_FUNDS", change: []}
+      };
+    }
+    return {status: "CLOSED", change: change}
     //mientras el cambio sea diferente de 0 hacer:
-        //encontrar el cambio que sea igual o menor al vuelto.
+        //encontrar el cambio que sea igual o menor al vuelto recorriendo todos los valores posibles de la caja registradora
             //si hay vuelto de ese valor (tipo de moneda) en la caja
                 //restarlo de la caja
                 //restarlo del vuelto
+                //agregarlo a la lista de billetes o modenas utilizadas
     //devolver CLOSE (porque salio del while y el cambio es 0)
         
     return change;
