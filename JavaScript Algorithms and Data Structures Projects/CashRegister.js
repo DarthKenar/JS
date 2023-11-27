f4c4d83719e5374c3e33e514b5881d2377ab240e
Federico <federico_vega22@hotmail.com>
2023-11-27 14:03:48 -0300

    CashReguster

--- JavaScript Algorithms and Data Structures Projects/CashRegister.js
+++ JavaScript Algorithms and Data Structures Projects/CashRegister.js
@@ -5,17 +5,17 @@ function checkCashRegister(price, cash, cid) {
   ["NICKEL", 0.05],
   ["DIME", 0.1],
   ["QUARTER", 0.25],
-  ["ONE", 1],
-  ["FIVE", 5],
-  ["TEN", 10],
-  ["TWENTY", 20],
-  ["ONE HUNDRED", 100]
+  ["ONE", 1.0],
+  ["FIVE", 5.0],
+  ["TEN", 10.0],
+  ["TWENTY", 20.0],
+  ["ONE HUNDRED", 100.0]
 ];
   const change = [
-  ["PENNY", 0.00],
-  ["NICKEL", 0.00],
-  ["DIME", 0.0],
-  ["QUARTER", 0.0],
+  ["PENNY", 0],
+  ["NICKEL", 0],
+  ["DIME", 0],
+  ["QUARTER", 0],
   ["ONE", 0],
   ["FIVE", 0],
   ["TEN", 0],
@@ -23,12 +23,13 @@ function checkCashRegister(price, cash, cid) {
   ["ONE HUNDRED", 0]
 ];
   function recursive(remainingChange, cid, cu, change){
-    for(let i=cid.length-1; i>0; i--){
+    for(let i=cid.length-1; i>=0; i--){
       if(currencyUnit[i][1]<=remainingChange){
         if(cid[i][1]>0){
           remainingChange -= currencyUnit[i][1]
           cid[i][1] -= currencyUnit[i][1]
           change[i][1] += currencyUnit[i][1]
+          remainingChange = remainingChange.toFixed(2)
           return recursive(remainingChange, cid, cu, change)
         };
       };
@@ -39,7 +40,7 @@ function checkCashRegister(price, cash, cid) {
   let response = recursive(remainingChange, cid, currencyUnit, change)
 
   let reduceCID = cid.reduce((accumulator, currentValue)=>accumulator + currentValue[1])
-  if(reduceCID > response[0]){
+  if(reduceCID < response[0]){
     return {status:"INSUFFICIENT_FUNDS",change:[]}
   }else if(reduceCID == 0 && remainingChange == 0){
     return {status: "CLOSED", change: response[1]}
@@ -48,5 +49,5 @@ function checkCashRegister(price, cash, cid) {
   };
 }
 
-var test = checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
-console.log(test)
+var test2 = checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
+console.log(test2)
