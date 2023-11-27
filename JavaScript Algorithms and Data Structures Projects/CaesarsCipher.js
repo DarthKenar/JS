15331fce2e75d5e402b739b0081da597da9ded07
Federico Vega <federico_vega22@hotmail.com>
2023-11-27 17:21:56 -0300

    CC

--- JavaScript Algorithms and Data Structures Projects/CaesarsCipher.js
+++ JavaScript Algorithms and Data Structures Projects/CaesarsCipher.js
@@ -1,18 +1,19 @@
 function rot13(str) {
-    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let strResponse = "";
-  
-    for (let i = 0; i < str.length; i++) {
-      if (/^[a-zA-Z]+$/.test(str[i])) {
-        let newIndex = (alphabet.indexOf(str[i]) + 13) % 26;
-        strResponse = strResponse.concat(alphabet[newIndex]);
-      } else {
-        strResponse = strResponse.concat(str[i]);
-      }
+  let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let strResponse = "";
+
+  for (let i = 0; i < str.length; i++) {
+    if (/^[A-Z]+$/.test(str[i])) {
+      let newIndex = (alphabet.indexOf(str[i]) + 13) % 26;
+      console.log(newIndex)
+      strResponse = strResponse.concat(alphabet[newIndex]);
+    } else {
+      strResponse = strResponse.concat(str[i]);
     }
-  
-    console.log(strResponse);
-    return strResponse;
   }
-  
-  rot13("SERR PBQR PNZC");  // "FREE CODE CAMP"
+
+  console.log(strResponse);
+  return strResponse;
+}
+
+rot13("SERR PBQR PNZC");
