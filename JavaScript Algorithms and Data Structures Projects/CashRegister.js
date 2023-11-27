7e5c10119aa73a6424185854b0e2f117896ea73f
Federico Vega <federico_vega22@hotmail.com>
2023-11-27 19:22:42 -0300

    CashRegister

--- JavaScript Algorithms and Data Structures Projects/CashRegister.js
+++ JavaScript Algorithms and Data Structures Projects/CashRegister.js
@@ -1,63 +1,52 @@
 function checkCashRegister(price, cash, cid) {
-  let remainingChange = cash - price;
-  const currencyUnit = [
-    ["PENNY", 0.01],
-    ["NICKEL", 0.05],
-    ["DIME", 0.1],
-    ["QUARTER", 0.25],
-    ["ONE", 1.0],
-    ["FIVE", 5.0],
-    ["TEN", 10.0],
-    ["TWENTY", 20.0],
-    ["ONE HUNDRED", 100.0]
+  var change = cash - price;
+  
+  var coinValues = [
+    { name: 'ONE HUNDRED', val: 100.00 },
+    { name: 'TWENTY', val: 20.00 },
+    { name: 'TEN', val: 10.00 },
+    { name: 'FIVE', val: 5.00 },
+    { name: 'ONE', val: 1.00 },
+    { name: 'QUARTER', val: 0.25 },
+    { name: 'DIME', val: 0.10 },
+    { name: 'NICKEL', val: 0.05 },
+    { name: 'PENNY', val: 0.01 }
   ];
-  const change = [
-    ["PENNY", 0],
-    ["NICKEL", 0],
-    ["DIME", 0],
-    ["QUARTER", 0],
-    ["ONE", 0],
-    ["FIVE", 0],
-    ["TEN", 0],
-    ["TWENTY", 0],
-    ["ONE HUNDRED", 0]
-  ];
-  function recursive(remainingChange, cid, cu, change){
-    for(let i=cid.length-1; i>=0; i--){
-      if(currencyUnit[i][1]<=remainingChange){
-        if(cid[i][1]>0){
-          remainingChange -= currencyUnit[i][1].toFixed(2)
-          cid[i][1] -= currencyUnit[i][1]
-          change[i][1] += currencyUnit[i][1]
-          remainingChange = remainingChange.toFixed(2)
-          return recursive(remainingChange, cid, cu, change)
-        };
-      };
-    };
-    return [remainingChange, change, cid]
+
+  var totalmoney = cid.reduce(function (x, y) {
+    return x + y[1];
+  }, 0.0);
+
+  if (totalmoney < change) {
+    return {status: "INSUFFICIENT_FUNDS", change: []};
+  };
+  
+  if (totalmoney === change) {
+    return {status: "CLOSED", change: cid};
   };
 
-  let response = recursive(remainingChange, cid, currencyUnit, change)
-  let reduceCID = cid.reduce((accumulator, currentValue)=>accumulator + currentValue[1],0).toFixed(2);
-  let reduceCID2 = response[1].reduce((accumulator, currentValue)=>accumulator + currentValue[1],0).toFixed(2);
-  let reduceCID3 = response[2].reduce((accumulator, currentValue)=>accumulator + currentValue[1],0).toFixed(2);
-  if(response[0] > 0){
-    return {status:"INSUFFICIENT_FUNDS",change:[]};
-  }else if(reduceCID3 == 0 && response[0] == 0){
-    return {
-      status: "CLOSED", 
-      change: response[1].map(
-        function(e){
-          if(e[1]>0){
-            return [e[0],e[1].toFixed(1)]
-          };
-          return [e[0],e[1]]
-        }
-      )};
-  }else{
-    return {status: "OPEN", change: response[1].reverse().filter((e) => e[1]>0)};
+  var reversecid = cid.slice().reverse();
+
+  var bills = coinValues.reduce(function (acc, next, index) {
+    if (change >= next.val) {
+      var coins = 0.0;
+      while (change >= next.val && reversecid[index][1] >= next.val) {
+        change -= next.val;
+        change = change.toFixed(2);
+        reversecid[index][1] -= next.val;
+        coins += next.val;
+      }
+      acc.push([next.name, coins]);
+      return acc;
+    }
+    else {
+      return acc;
+    }
+  }, []);
+
+  if (change % totalmoney !== 0) {
+    return {status: "INSUFFICIENT_FUNDS", change: []};
   };
+
+  return {status: "OPEN", change: bills};
 };
-
-var test2 = checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
-console.log(test2);
