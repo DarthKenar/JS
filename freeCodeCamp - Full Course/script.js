1f291d24f2e8267f0042dfee7af1c6d7050d51fa
Federico <federico_vega22@hotmail.com>
2023-11-30 15:55:49 -0300

    terminado neumorfismo ejemplo

--- freeCodeCamp - Full Course/script.js
+++ freeCodeCamp - Full Course/script.js
@@ -1,29 +1,44 @@
-function pressBtn(){
+function btnStyle(btn){
     btn.classList.toggle("button");
     btn.classList.toggle("button-pressed");
-    setTimeout(function(){
-        btn.classList.toggle("button-pressed");
-        btn.classList.toggle("button");
-    },100);
-}
-
-let btnIncrement = document.getElementById("btn-increment")
-let btnSave = document.getElementById("btn-save")
+};
 
 function incrementCounter(){
     let counter = document.getElementById("counter")
-    let value = 0
     value += 1
     counter.textContent = value
 };
 
-function save(){
-    let value = 0
+function saveCounter(){
     let log = document.getElementById("log")
     log.textContent += ` ${value} -`
+    value = 0
     counter.textContent = value
 };
 
-//Styles
+function btnIncrementActionDown(btn){
+    btnStyle(btn);
+    incrementCounter();
+};
+function btnIncrementActionUp(btn){
+    btnStyle(btn);
+};
+
+function btnSaveActionDown(btn){
+    btnStyle(btn);
+    saveCounter();
+};
+function btnSaveActionUp(btn){
+    btnStyle(btn);
+};
+
+let value = 0
+let btnIncrement = document.getElementById("btn-increment")
+let btnSave = document.getElementById("btn-save")
+btnIncrement.addEventListener("mousedown",()=>(btnIncrementActionDown(btnIncrement)))
+btnIncrement.addEventListener("mouseup",()=>(btnIncrementActionUp(btnIncrement)))
+btnSave.addEventListener("mousedown",()=>(btnSaveActionDown(btnSave)))
+btnSave.addEventListener("mouseup",()=>(btnSaveActionUp(btnSave)))
+
 
 
