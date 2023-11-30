810e4da23de4a2ca879adbef12e097fa230b3c31
Federico <federico_vega22@hotmail.com>
2023-11-30 11:22:15 -0300

    Preparando todo para hacer clickeable los dos botones increment y save. Preparo para hacer que el botón se muestre presionado y despresionado correctamente

--- freeCodeCamp - Full Course/script.js
+++ freeCodeCamp - Full Course/script.js
@@ -1,20 +1,29 @@
-let value = 0
-function increment(){
+function pressBtn(){
+    btn.classList.toggle("button");
+    btn.classList.toggle("button-pressed");
+    setTimeout(function(){
+        btn.classList.toggle("button-pressed");
+        btn.classList.toggle("button");
+    },100);
+}
+
+let btnIncrement = document.getElementById("btn-increment")
+let btnSave = document.getElementById("btn-save")
+
+function incrementCounter(){
     let counter = document.getElementById("counter")
+    let value = 0
     value += 1
     counter.textContent = value
 };
+
 function save(){
+    let value = 0
     let log = document.getElementById("log")
     log.textContent += ` ${value} -`
-    value = 0
     counter.textContent = value
 };
 
 //Styles
-function incrementStyle(){
-    let btn = document.getElementsByClassName("btn-increment")
-    btn.classList.toggle("button");
-    btn.classList.toggle("button-pressed");
-}
+
 
